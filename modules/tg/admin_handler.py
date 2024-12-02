bf429c07d0dd80be43613bb27118b688e8cb41ec
Ильфат <kilfat35@gmail.com>
2024-12-02 21:57:47 +0500

    admin handlers update

--- modules/tg/admin_handler.py
+++ modules/tg/admin_handler.py
@@ -1,3 +1,4 @@
+from aiogram import html
 from aiogram.filters import BaseFilter, Command
 from aiogram.types import Message, FSInputFile
 from modules.tg.config import ADMIN_ID, log_file
@@ -11,9 +12,14 @@ class IsAdmin(BaseFilter):
         self.admin_id = int(ADMIN_ID)
     async def __call__(self, message: Message) -> bool:
         return message.from_user.id == int(ADMIN_ID)
+
+@admin_router.message(IsAdmin(), Command('start'))
+async def admin_start_handler(message: Message) -> None:
+    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}!\nЯ нейросеть для распознавания факта курения.\n\nОтправь мне фотографию и я вынесу свой вердикт.\n\n/credits - информация\n\nВам доступна админ панель:\n/admin - Команды администрирования")
+
 @admin_router.message(IsAdmin(), Command('admin'))
 async def admin_handler(message: Message) -> None:
-    await message.answer("Админ обработчик сработал")
+    await message.answer("Админ панель:\n\n/logs - отправить лог-файл")
 
 @admin_router.message(IsAdmin(), Command('logs'))
 async def log_sender(message: Message) -> None:
